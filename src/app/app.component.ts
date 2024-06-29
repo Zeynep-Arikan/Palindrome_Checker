001a7abe2389b5ae451c7e06a45c2c667359588b
Zeynep ARIKAN <zynparkn2110@gmail.com>
2024-06-29 21:33:06 +0300

    palindrome project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// temel direktifler ngFor,nglf vs
+import { CommonModule } from '@angular/common';
+// ngModel için gerekli form işlevselliği sağlar
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]  // FormsModule burada ekleniyor
 })
 export class AppComponent {
-  title = 'palindrom';
+  title = 'Palindrome Checker';
+  userInput = '';
+  palindromeData = [
+    { text: "racecar", isPurePalindrome: true, isPalindrome: true },
+    { text: "A man, a plan, a canal: Panama", isPurePalindrome: false, isPalindrome: true },
+    { text: "test", isPurePalindrome: false, isPalindrome: false }
+  ];
+
+  // check is palindrome or not
+  addPalindrome() {
+    const cleanedText = this.userInput.replace(/[\W_]/g, '').toLowerCase();
+    const reversedText = cleanedText.split('').reverse().join('');
+    // cleanedText is compared with reversedText
+    const isPalindrome = cleanedText === reversedText;
+    // The original text is reversed and compared with the original.
+    const isPurePalindrome = this.userInput === this.userInput.split('').reverse().join('');
+
+    this.palindromeData.push({
+      text: this.userInput,
+      isPurePalindrome: isPurePalindrome,
+      isPalindrome: isPalindrome
+    });
+
+    this.userInput = ''; // Input alanını temizle
+  }
 }
